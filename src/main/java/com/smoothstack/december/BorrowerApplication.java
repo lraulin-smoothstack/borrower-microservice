4667989431c61b8fe5af84f86fa4fbcc47887326
Lee Raulin <leeraulin@gmail.com>
2020-01-14 15:04:22 -0500

    fixing dependencies

--- src/main/java/com/smoothstack/december/BorrowerApplication.java
+++ src/main/java/com/smoothstack/december/BorrowerApplication.java
@@ -1,13 +1,15 @@
 package com.smoothstack.december;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableAutoConfiguration
+public class BorrowerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BorrowerApplication.class, args);
 	}
 
 }
