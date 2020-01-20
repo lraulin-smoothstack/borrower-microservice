2095f9d1295261b887df253473f0860610b0d86a
Lee Raulin <leeraulin@gmail.com>
2020-01-20 17:46:58 -0500

    gitignore fix

--- src/main/java/com/smoothstack/december/BorrowerApplication.java
+++ src/main/java/com/smoothstack/december/BorrowerApplication.java
@@ -3,9 +3,12 @@ package com.smoothstack.december;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableFeignClients
 public class BorrowerApplication {
 
 	public static void main(String[] args) {
