aa68a67d2612a8b194d4fcf45b9e671c6491668b
Lee Raulin <leeraulin@gmail.com>
2020-01-20 21:16:49 -0500

    ...

--- src/main/java/com/smoothstack/december/BorrowerApplication.java
+++ src/main/java/com/smoothstack/december/BorrowerApplication.java
@@ -1,14 +1,11 @@
 package com.smoothstack.december;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class BorrowerApplication {
 
 	public static void main(String[] args) {
