5565bd7155484df93a303ec4c5087912ddbd52e1
Lee Raulin <leeraulin@gmail.com>
2020-01-21 20:57:22 -0500

    Fix apis

--- src/main/java/com/smoothstack/december/controller/BorrowerController.java
+++ src/main/java/com/smoothstack/december/controller/BorrowerController.java
@@ -10,35 +10,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("v1/lms/borrower-service")
 public class BorrowerController {
     @Autowired
     BorrowerService borrowerService;
 
-    @PostMapping("/checkout")
-    public void checkOutBook(@RequestParam("bookId") long bookId, @RequestParam("branchId") long branchId,
-            @RequestParam("borrowerId") long borrowerId) {
+    @PostMapping("/borrowers/{borrowerId}/branches/{branchId}/books/{bookId}:checkout")
+    public void checkOutBook(@PathVariable("bookId") long bookId, @PathVariable("branchId") long branchId,
+            @PathVariable("borrowerId") long borrowerId) {
         borrowerService.checkOutBook(bookId, branchId, borrowerId);
     }
 
-    @PostMapping("/checkin")
+    @PostMapping("/borrowers/{borrowerId}/branches/{branchId}/books/{bookId}:checkin")
     public void checkInBook(@RequestParam("bookId") long bookId, @RequestParam("branchId") long branchId,
             @RequestParam("borrowerId") long borrowerId) {
         borrowerService.checkInBook(bookId, branchId, borrowerId);
     }
 
-    @GetMapping("/library-branches")
+    @GetMapping("/branches")
     public List<LibraryBranch> getLibraryBranches() {
         return borrowerService.getLibraryBranches();
     }
 
-    @GetMapping("/branch-{:branchId}")
+    @GetMapping("/borrowers/{borrowerId}/branches/{branchId}/available-books/")
     public List<Book> getAvailableBooksNotCheckedOut(@PathVariable("branchId") long branchId,
-            @RequestParam("borrowerId") long borrowerId) {
+            @PathVariable("borrowerId") long borrowerId) {
         return borrowerService.getAvailableBooksNotCheckedOut(branchId, borrowerId);
     }
 
-    @GetMapping("/branch-{:branchId}/borrower-{:borrowerId}")
+    @GetMapping("/branches/{branchId}/borrowers/{borrowerId}")
     public List<BookLoan> getBookLoans(@PathVariable("branchId") long branchId,
             @PathVariable("borrowerId") long borrowerId) {
         return borrowerService.getBookLoansForBorrower(branchId, borrowerId);
