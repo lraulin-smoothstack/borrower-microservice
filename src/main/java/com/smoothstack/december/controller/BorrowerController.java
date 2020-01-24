98fcb44b10124f7111ec9f3c272a76518894e1bb
Lee Raulin <leeraulin@gmail.com>
2020-01-24 11:11:52 -0500

    Add log4j logger

--- src/main/java/com/smoothstack/december/controller/BorrowerController.java
+++ src/main/java/com/smoothstack/december/controller/BorrowerController.java
@@ -6,12 +6,17 @@ import com.smoothstack.december.entity.Book;
 import com.smoothstack.december.entity.BookLoan;
 import com.smoothstack.december.entity.LibraryBranch;
 import com.smoothstack.december.service.BorrowerService;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("v1/lms/borrower-service")
 public class BorrowerController {
+    private static final Logger logger = LogManager.getLogger(BorrowerController.class);
+
     @Autowired
     BorrowerService borrowerService;
 
