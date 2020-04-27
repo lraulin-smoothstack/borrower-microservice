0c63e3f052dcba9875be9dac9432e9c04552ca84
timothy-breitenfeldt <timothy.breitenfeldt@smoothstack.com>
2020-04-26 21:38:04 -0400

    Modified service to use eureka.

--- src/main/java/com/smoothstack/lms/borrowerservice/exception/ResourceAlreadyExistsException.java
+++ src/main/java/com/smoothstack/lms/borrowerservice/exception/ResourceAlreadyExistsException.java
@@ -2,6 +2,8 @@ package com.smoothstack.lms.borrowerservice.exception;
 
 public class ResourceAlreadyExistsException extends RuntimeException {
 
+    private static final long serialVersionUID = -9193996446984493423L;
+
     public ResourceAlreadyExistsException(String message) {
         super(message);
     }
--- src/main/resources/sql/mock_data.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE insert_book
-(IN title VARCHAR(255),
-publisher VARCHAR(255))
-BEGIN
-  SELECT Name, HeadOfState FROM Country
-  WHERE Continent = con;
-END //
-DELIMITER ;
